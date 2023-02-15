496ef2a2c99ec29ae0fc65c56f15991dfec58e33
ninh phạm <phamninh708@gmail.com>
2023-02-15 23:03:40 +0700

    first commit

---
+++ LearnWPF_Advanced/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LearnWPF_Advanced
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnApply_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"This description is :{tbDescription.Text}");
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            ckbWeld.IsChecked = ckbAssembly.IsChecked = ckbPlasma.IsChecked =
+                ckbLaser.IsChecked = ckbPurchase.IsChecked = ckbLathe.IsChecked =
+                ckbDrill.IsChecked = ckbFold.IsChecked = ckbRoll.IsChecked = 
+                ckbSaw.IsChecked = false;
+        }
+    }
+}
